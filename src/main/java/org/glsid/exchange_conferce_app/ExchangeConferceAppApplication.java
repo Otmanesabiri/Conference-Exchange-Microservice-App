ead629dc9c7c86c0213e6ff545637d8bc1809ede
OTMANE <othmandak87@gmail.com>
2025-10-20 09:34:59 +0100

    first commit

---
+++ src/main/java/org/glsid/exchange_conferce_app/ExchangeConferceAppApplication.java
@@ -0,0 +1,13 @@
+package org.glsid.exchange_conferce_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeConferceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeConferceAppApplication.class, args);
+	}
+
+}
